143da2e52664767aee5a81e7822967ae6f8421c1
yuemori <yuemori@aiming-inc.com>
2019-09-08 23:23:02 +0900

    Implement Fibonacci result

--- app/models/fibonacci.rb
+++ app/models/fibonacci.rb
@@ -1,2 +1,14 @@
 class Fibonacci < ApplicationRecord
+  def result
+    calculate(number)
+  end
+
+  private
+
+  def calculate(n)
+    return   if n < 0
+    return n if n < 2
+
+    calculate(n - 1) + calculate(n - 2)
+  end
 end
