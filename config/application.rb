5076a97da64fb25910d186db67e78ca242b15c7a
yuemori <yuemori@aiming-inc.com>
2019-09-08 23:09:03 +0900

    Introduce rspec-rails, factory_bot_rails

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,18 @@ module PerformanceTestApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.test_framework(
+        :rspec,
+        fixture: true,
+        request_specs: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true
+      )
+
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
+    end
   end
 end
